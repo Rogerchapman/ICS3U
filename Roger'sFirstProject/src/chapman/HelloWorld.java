bfffe8854bd4a6f4c334afc30abb8ed66e8a10cc
Roger Chapman <rogerchapman777@gmail.com>
2017-02-21 11:23:44 -0500

    The first program known as Hello World is what I, Roger Chapman, am pushing on this fine day in late Febraury.

---
+++ Roger'sFirstProject/src/chapman/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package chapman;
+
+/**
+ * HelloWorld.java
+ * First program for Java
+ * February 21 2017
+ * @author Roger Chapman
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
